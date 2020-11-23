d1e598208952fa48d0289a0c55d26e7c479a1d92
Paige Peck <ppeck@citadel.edu>
2020-11-23 17:16:52 +0000

    Added logout button on admin page.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -25,4 +25,9 @@ class AdminController < ApplicationController
   def reset    
 #       redirect_to admin_index_path
   end
+    
+  def logout
+    session.delete(:hash)
+    redirect_to homepage_path
+  end
 end

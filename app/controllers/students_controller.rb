41254a39df6bbe1d2e18b1f0e34468ae69765a4c
Arthur Gartner <40064946+ArthurGartner@users.noreply.github.com>
2020-11-30 22:55:51 -0500

    Update students_controller.rb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,7 @@ class StudentsController < ApplicationController
   def edit
     @student = Student.find params[:id]
 
+    #Attempt to find session from database that matches sessionhash in session
     user_session = Session.find_by(sessionhash: session[:hash])
     user_account_id = user_session.account_id
     user_account = Account.find(user_account_id)
@@ -15,16 +16,19 @@ class StudentsController < ApplicationController
     student_account = Student.find(params[:id])
     #puts "Student ID: #{student_account.id}"
 
+    #Redirect if different or no hash is found
     if student_account.id != user_account.account_id
         #puts "Account ID: #{user_account.accountId} for user #{user_account.username}"
         redirect_to '/'
     end
 
     @date = Date.today
+    # Get all 12 months
     @months = []
     (1..12).each do |m|
       @months << [Date::MONTHNAMES[m]]
     end
+    # Get next 10 years
     @years = []
     (0..9).each do |y|
       @years << [@date.next_year(y).strftime("%Y")]
@@ -40,20 +44,24 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find params[:id]
 
+    # If no session found then redirect to root
     user_session = Session.find_by(sessionhash: session[:hash])
     if user_session == nil then
       redirect_to '/' and return
     end
     user_account_id = user_session.account_id
     user_account = Account.find(user_account_id)
+    #If no account found then clear session and redirect
     if user_account == nil then
       redirect_to '/logoutredirect'
     end
 
+    #If student account then view of other student profiles are limited
     if user_account.accountType == 0
       student_account = Student.find(params[:id])
       puts "Student ID: #{student_account.id}"
 
+      #Redirect if logged in student does not match student viewed
       if student_account.id != user_account.account_id
         puts "Account ID: #{user_account.account_id} for user #{user_account.username}"
         redirect_to '/' and return
@@ -61,7 +69,8 @@ class StudentsController < ApplicationController
     end
       
     @atype = user_account.accountType
-      
+     
+    #Redirect for url pointing to non existing record
     rescue ActiveRecord::RecordNotFound
     redirect_to '/', :flash => { :error => "Record not found." }
   end
